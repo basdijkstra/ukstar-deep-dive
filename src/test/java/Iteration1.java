367a548789f403a5e54bce29f566a623e4fb29e2
Bas Dijkstra <bas@ontestautomation.com>
2019-02-26 14:51:14 +0100

    Initial commit

---
+++ src/test/java/Iteration1.java
@@ -0,0 +1,45 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Iteration1 {
+
+    @Test
+    public void DoLoanRequest_UsingAmountsWithinLimits_ShouldBeAccepted() {
+
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("http://localhost:8080/parabank");
+
+        driver.findElement(By.name("username")).sendKeys("john");
+        driver.findElement(By.name("password")).sendKeys("demo");
+        driver.findElement(By.xpath("//input[@value='Log In']")).click();
+
+        driver.findElement(By.linkText("Request Loan")).click();
+
+        driver.findElement(By.id("amount")).sendKeys("1000");
+        driver.findElement(By.id("downPayment")).sendKeys("100");
+        Select fromAccountId = new Select(driver.findElement(By.id("fromAccountId")));
+        fromAccountId.selectByVisibleText("13122");
+        driver.findElement(By.xpath("//input[@value='Apply Now']")).click();
+
+        try {
+            Thread.sleep(2000);
+        }
+        catch(InterruptedException ie) {
+            Assert.fail("INTERRUPT");
+        }
+
+        String actualStatus = driver.findElement(By.id("loanStatus")).getText();
+
+        Assert.assertEquals("Approved", actualStatus);
+
+        driver.quit();
+    }
+}
