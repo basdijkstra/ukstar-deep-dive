f0c40102c54785b06f31b4d0d8ece77ac25bd5c1
Bas Dijkstra <bas@ontestautomation.com>
2019-02-26 15:16:53 +0100

    Added second and third iteration

---
+++ src/test/java/Iteration3.java
@@ -0,0 +1,67 @@
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import static io.restassured.RestAssured.given;
+
+public class Iteration2 {
+
+    private WebDriver driver;
+
+    @Before
+    public void initializeDatabase() {
+
+        given().
+        when().
+            post("http://localhost:8080/parabank/services/bank/initializeDB").
+        then().
+            log().
+            all();
+    }
+
+    @Before
+    public void initializeBrowser() {
+
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void DoLoanRequest_UsingAmountsWithinLimits_ShouldBeAccepted() {
+
+        driver.get("http://localhost:8080/parabank");
+
+        driver.findElement(By.name("username")).sendKeys("john");
+        driver.findElement(By.name("password")).sendKeys("demo");
+        driver.findElement(By.xpath("//input[@value='Log In']")).click();
+
+        driver.findElement(By.linkText("Request Loan")).click();
+
+        driver.findElement(By.id("amount")).sendKeys("1000");
+        driver.findElement(By.id("downPayment")).sendKeys("100");
+        Select fromAccountId = new Select(driver.findElement(By.id("fromAccountId")));
+        fromAccountId.selectByVisibleText("13122");
+        driver.findElement(By.xpath("//input[@value='Apply Now']")).click();
+
+        try {
+            Thread.sleep(2000);
+        }
+        catch(InterruptedException ie) {
+            Assert.fail("INTERRUPT");
+        }
+
+        String actualStatus = driver.findElement(By.id("loanStatus")).getText();
+
+        Assert.assertEquals("Approved", actualStatus);
+    }
+
+    @After
+    public void tearDown() {
+
+        driver.quit();
+    }
+}
